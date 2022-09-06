5355e445dfdd901a42126a49276d4ec7a075a809
Kamen Stanchev <k.s@abv.bg>
2022-09-06 08:51:32 +0300

    models

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    email = models.EmailField()
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField(max_length=200)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.FloatField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=20, null=True, blank=True)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    data_added = models.DateTimeField(auto_now_add=True)
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    address = models.TextField(max_length=50)
+    country = models.CharField(max_length=20)
+    zipcode = models.CharField(max_length=20)
+    date_added = models.DateTimeField(auto_now_add=True)
+
