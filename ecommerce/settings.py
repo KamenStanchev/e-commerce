e0d7e0289fb9594121d203514e05cff6d3a76eae
Kamen Stanchev <k.s@abv.bg>
2022-12-22 20:03:44 +0200

    allowed hosts

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', None) == 'True'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://kamen-ecommerce.up.railway.app']
 
 # TODO
 CSRF_TRUSTED_ORIGINS = []
